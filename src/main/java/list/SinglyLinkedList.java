55829476c565e84dd68d50070f1e0ea6dc10335f
Pham Hai Trung <haitrung.pham6573@gmail.com>
2020-11-09 10:24:45 +0100

    initial commit

--- src/main/java/list/SinglyLinkedList.java
+++ src/main/java/list/SinglyLinkedList.java
@@ -6,61 +6,78 @@ import java.util.Iterator;
 import java.util.function.Consumer;
 
 public class SinglyLinkedList<T> {
-    private int size = 0; // number of elements excluding the tail
-    private Node tail = null;
-    private Node head = null;
+    private int size = 0; // number of elements
+    private Node tail; // last element of the list
+    private Node head; // first element of the list
 
-    public void add(int index, T value) throws IndexOutOfBoundException {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundException();
+    public void add(T value) {
         Node newNode = createNewNode(value);
-        if (size <= 0) {
-            newNode.setNextElement(tail);
-
+        if (size == 0) {
+            newNode.nextElement = null;
+            head = newNode;
+            tail = newNode;
         } else {
-            for (int i = 0; i <= index; i++) {
+            newNode.nextElement = head;
+            head = newNode;
+        }
+        size++;
+    }
 
+    public void add(int index, T value) {
+        if (index < 0 && index > size) throw new IndexOutOfBoundsException();
+        if (index == 0) {
+            add(value);
+        } else {
+            Node nodeToAdd = createNewNode(value);
+            Node traversePointer = head;
+            int i = 0;
+            while (traversePointer.nextElement != null) {
+                if (i == index - 1) {
+                    nodeToAdd.nextElement = traversePointer.nextElement;
+                    traversePointer.nextElement = nodeToAdd;
+                }
+                i++;
+                traversePointer = traversePointer.nextElement;
             }
         }
         size++;
     }
 
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public T get(int index) {
+        int i = 0;
+        Node traversePointer = head;
+        T result = null;
+        while (traversePointer.nextElement != null) {
+            if (i == index) {
+                result = traversePointer.value;
+            }
+            i++;
+            traversePointer = traversePointer.nextElement;
+        }
+        return result;
+    }
+
+
     private Node createNewNode(T value) {
         return new Node(value);
-    }
-    public void remove(int element) {
-
     }
 
     public int size() {
         return size;
     }
 
-    class Node {
+    // internal class represents one element of the linked list
+    private class Node {
         private Node nextElement;
         private T value;
-        private int index;
 
         Node(T value) {
-            this.value = value;
-        }
-
-        private Node getNextElement() {
-            return nextElement;
-        }
-
-        private void setNextElement(Node nextElement) {
-            this.nextElement = nextElement;
-        }
-
-        private T getValue() {
-            return value;
-        }
-
-        private int getIndex() {
-            return index;
-        }
-
-        private void setValue(T value) {
             this.value = value;
         }
     }
--- src/test/java/list/SinglyLinkedListTest.java
+++ src/test/java/list/SinglyLinkedListTest.java
@@ -1,13 +1,29 @@
 package list;
 
+import org.junit.After;
 import org.junit.Test;
 
 
 public class SinglyLinkedListTest {
-    private SinglyLinkedList linkedList;
+    private SinglyLinkedList<Integer> linkedList = new SinglyLinkedList();
+
+    @After
+    public void clearList() {
+        linkedList.clear();
+    }
 
     @Test
     public void addOneElement() {
+        linkedList.add(4);
+        assert(linkedList.size() == 1);
+    }
 
+    @Test
+    public void addTwoElement() {
+        linkedList.add(5);
+        linkedList.add(6);
+        assert(linkedList.get(1) == 6);
+        assert(linkedList.get(0) == 5);
+        assert(linkedList.size() == 2);
     }
 }
